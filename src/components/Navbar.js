a1e1958d30719a92ba294e42ec70eb190a491b58
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-13 10:42:59 +0300

    adding is nav open

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,16 @@ const Navbar = () => {
             <div className="Nav-logo">
                 <h1>Logo</h1>
             </div>
+
+            <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                {['Work', 'About', 'Playground', 'Resource'].map((text, i) => (
+                    <li key={i}>
+                        <a href="#">
+                        <span className="roller">{text}</span>
+                        </a>
+                    </li>
+                    ))}
+            </ul>
         </div>
     )
 }
