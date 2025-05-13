d104ad4a68b51d8cd285e285068817265349dd03
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-13 21:48:22 +0300

    imported products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Products />
     </div>
   );
 }
