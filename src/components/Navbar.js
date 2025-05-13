08fa4b4758176a7c401c06313792252017304416
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-13 10:42:00 +0300

    adding the class names

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return(
+        <div className="mordern-navbar">
+            <div className="Nav-logo">
+                <h1>Logo</h1>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
