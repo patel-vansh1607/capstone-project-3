0565bd38b55d881da8093f96cce50109f76de347
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-15 20:45:23 +0300

    added

---
+++ src/pages/Mobile.js
@@ -0,0 +1,10 @@
+import MobileDeals from '../components/Mobile/MobileDeals';
+
+const Mobile = () => {
+    return(
+        <div>
+            <MobileDeals />
+        </div>
+    )
+}
+export default Mobile;
