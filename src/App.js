c8f196603d6c220c854394cf2050b218fe6dbb1e
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-15 10:12:20 +0300

    importted compoent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Carousel from './components/Carousel';
 import Navbar from './components/Navbar';
+import NavList from './components/NavList';
 import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Carousel />
+      <NavList />
     </div>
   );
 }
