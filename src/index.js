596dc5f6954ad1a57c9db6f0d2a33c1125b732cd
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-15 20:44:46 +0300

    fixing browser routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Mobile from './pages/Mobile';
+import PhoneDetails from './components/Mobile/PhoneDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,7 +14,14 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    
+  },
+  {
+    path: "/electronics/mobiles",
+    element: <Mobile />,
+  },
+  {
+    path: "/phone/:id", 
+    element: <PhoneDetails />,
   },
 ])
 root.render(
@@ -25,3 +34,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// /phone/itel-a50
