d69452875a0c2b71a5cd1014c70cb59bb9e0c39f
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-15 21:34:40 +0300

    add

--- src/pages/Mobile.js
+++ src/pages/Mobile.js
@@ -1,9 +1,13 @@
+import Footer from '../components/Footer';
 import MobileDeals from '../components/Mobile/MobileDeals';
+import Navbar from '../components/Navbar';
 
 const Mobile = () => {
     return(
         <div>
+            <Navbar />
             <MobileDeals />
+            <Footer />
         </div>
     )
 }
