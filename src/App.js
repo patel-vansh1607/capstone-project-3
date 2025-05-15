aa4246dc999a458a595726e4374ecfa013a0ccdf
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-15 18:18:21 +0300

    adding component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Navlist from './components/Navlist';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Navlist />
       <Footer />
     </div>
   );
