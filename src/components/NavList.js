dbd85b57c8d516ec707d590349f0b2d186a915ee
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-15 09:45:06 +0300

    adding the skeleton of the code

---
+++ src/components/NavList.js
@@ -0,0 +1,12 @@
+import "../styles/NavList.css";
+
+const NavList = () => {
+    return(
+        <div className="navbar">
+            <div className="nav-list">
+
+            </div>
+        </div>
+    )
+}
+export default NavList;
