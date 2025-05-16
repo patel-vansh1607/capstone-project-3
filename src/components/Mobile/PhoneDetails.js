54bd68e91df813fccb55a2d484deb53e4e676e16
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-16 10:29:57 +0300

    imported styles

--- src/components/Mobile/PhoneDetails.js
+++ src/components/Mobile/PhoneDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import phones from "../../data/phonedata";
-import "./MobileDeals.css";
+import "../../styles/MobileDeals.css";
 
 const PhoneDetails = () => {
     const { id } = useParams();
