ff4fc7e184d93b371c0c923b57160895258aab07
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-16 10:31:35 +0300

    added

---
+++ src/pages/Laptop.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import LaptopDeals from '../components/Laptop/LaptopDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <LaptopDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
