3ddef49a6831696d510f0081169ac9a19be7d4a1
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 19:53:08 +0300

    feat: create Mobile component for MensJeans page layout

---
+++ src/pages/MensJeans.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import MensJeansDeals from '../components/MensJeans/MensJeansDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <MensJeansDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
