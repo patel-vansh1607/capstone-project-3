591560fe506fc04ef37e0fd86fb1b2df3e56e2e6
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 09:29:37 +0300

    feat: add Refrigerator component to display refrigerator deals

---
+++ src/pages/Refrigerator.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import RefrigeratorDeals from '../components/Refrigerator/RefrigeratorDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <RefrigeratorDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
