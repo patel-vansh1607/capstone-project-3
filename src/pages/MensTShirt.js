45fc34c500b10a1f310cde6b4d1986da793d6573
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 17:31:57 +0300

    feat: create Mobile component for MensTShirt page layout

---
+++ src/pages/MensTShirt.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import MensTShirtDeals from '../components/MensTShirt/MensTShirtDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <MensTShirtDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
