faedd0e51d51aa8ade7a48aa4e1fd5a037a34961
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 20:33:39 +0300

    feat: add laptop data for women's tops to the application

---
+++ src/data/womenstop.js
@@ -0,0 +1,30 @@
+const laptop = [
+  {
+    id: "zara-crop-top",
+    name: "ZARA Women's Ribbed Crop Top",
+    image: "zara-crop-top.jpeg",
+    price: 799,
+    originalPrice: 1199,
+    discount: "-33%",
+    brand: "ZARA",
+    warranty: "No Warranty",
+    description: "Cotton Blend, Sleeveless, Fitted Style",
+    shipping: "Delivery by 27 May, Monday",
+    rating: 4.4,
+  },
+  {
+    id: "h&m-floral-top",
+    name: "H&M Women's Floral Blouse",
+    image: "hm-floral-top.jpeg",
+    price: 999,
+    originalPrice: 1399,
+    discount: "-29%",
+    brand: "H&M",
+    warranty: "No Warranty",
+    description: "Chiffon Fabric, Puff Sleeves, Printed",
+    shipping: "Delivery by 28 May, Tuesday",
+    rating: 4.5,
+  }
+];
+
+export default laptop;
