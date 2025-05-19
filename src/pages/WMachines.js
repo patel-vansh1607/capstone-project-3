2821501ae6f89097cb5365232bb35fa333aa4aec
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 09:46:54 +0300

    feat: create WMachines page to display washing machines deals

---
+++ src/pages/WMachines.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import WMachinesDeals from '../components/WMachines/WMachinesDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <WMachinesDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
