ba08beb59579dbedca1f2e42d5d29561fa6338e0
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 09:51:45 +0300

    feat: create Camera page to display microwave deals

---
+++ src/pages/Microwave.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import MicrowaveDeals from '../components/Microwave/MicrowaveDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <MicrowaveDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
