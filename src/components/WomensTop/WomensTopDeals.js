7b7ed3348b1503ac62e65b2511aed338cbad4a9b
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 20:33:31 +0300

    feat: add LaptopDeals component for displaying deals on women's tops

---
+++ src/components/WomensTop/WomensTopDeals.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import phones from "../../data/womenstop";
+import "../../styles/MobileDeals.css";
+
+const LaptopDeals = () => {
+  return (
+    <div className="deals-container">
+      {phones.map((phone) => (
+        <Link to={`/jeans/${phone.id}`} key={phone.id} className="phone-card">
+          {phone.discount && <span className="discount-badge">{phone.discount}</span>}
+          <img src={phone.image} alt={phone.name} className="phone-image" />
+          <h3 className="phone-name">{phone.name}</h3>
+          <p className="phone-price">KSh {phone.price.toLocaleString()}</p>
+          {phone.originalPrice && (
+            <p className="phone-original">KSh {phone.originalPrice.toLocaleString()}</p>
+          )}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default LaptopDeals;
