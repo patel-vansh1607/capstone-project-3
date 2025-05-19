caedf6b4dc1279814344f29de08a29bb8ab161e5
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 20:33:43 +0300

    feat: create Mobile component for Women's Tops page layout

---
+++ src/pages/WomensTop.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import WomensTopDeals from '../components/WomensTop/WomensTopDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <WomensTopDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
