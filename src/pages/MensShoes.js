9deae8a732820664590ab67696ad89c9c89132ae
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-19 20:27:50 +0300

    feat: create Mobile component for MensShoes page layout

---
+++ src/pages/MensShoes.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import MensShoesDeals from '../components/MensShoes/MensShoesDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <MensShoesDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
