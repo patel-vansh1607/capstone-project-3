8e367c064487a76ad04b4193328cbcc0a83f4392
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 21:57:36 +0300

    feat: create Mobile component to display fitness deals with Navbar and Footer

---
+++ src/pages/Fitness.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import FitnessDeals from '../components/Fitness/FitnessDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <FitnessDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
