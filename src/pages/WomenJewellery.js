48e0b12e0f88ee74cb46b10315c1ab519da6d24c
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 13:22:55 +0300

    feat: create Mobile component for displaying women's jewellery deals with Navbar and Footer

---
+++ src/pages/WomenJewellery.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import WomensJewelleryDeals from '../components/WomensJewellery/WomensJewelleryDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <WomensJewelleryDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
