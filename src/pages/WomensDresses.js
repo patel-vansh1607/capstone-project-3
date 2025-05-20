905fb276ccceba4bbeaa9a94416568eb375408ad
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 13:11:06 +0300

    feat: add Camera component to display women's dresses with Navbar and Footer

---
+++ src/pages/WomensDresses.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import WomenDressesDeals from '../components/WomensDresses/WomensDressesDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <WomenDressesDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
