35218d6a7c1e8fb903edbb9e302967042532444c
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 13:30:41 +0300

    feat: create Mobile page for displaying women's handbags deals with Navbar and Footer

---
+++ src/pages/WomensHandbag.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import WomensHandbagsDeals from '../components/WomensHandbags/WomensHandbagsDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <WomensHandbagsDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
