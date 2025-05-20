d8d5c4f6cb83259c94796b38bc8a2bab930944f2
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 13:43:12 +0300

    feat: create Mobile component for displaying kids toys deals with Navbar and Footer

---
+++ src/pages/KidsToys.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import KidsToysDeals from '../components/KidsToys/KidsToysDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <KidsToysDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
