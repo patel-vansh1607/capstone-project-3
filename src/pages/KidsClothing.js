b19385000e67b98c0ca2459050e0d47a0a52fbfd
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 13:38:34 +0300

    feat: create Mobile page for displaying kids clothing deals with Navbar and Footer

---
+++ src/pages/KidsClothing.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import KidsClothingDeals from '../components/KidsClothing/KidsClothingDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <KidsClothingDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
