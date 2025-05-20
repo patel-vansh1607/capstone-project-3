4527edae60354c3cc4e24604869890d2dda3bdd8
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 18:24:06 +0300

    feat: add Camera component to display sofas deals with Navbar and Footer

---
+++ src/pages/Sofas.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import SofasDeals from '../components/Sofas/SofasDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <SofasDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
