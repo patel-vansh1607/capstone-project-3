052254446f24b742696371da7f62a3105519aad6
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 18:12:22 +0300

    feat: add Camera component to display beds deals layout

---
+++ src/pages/Beds.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import BedsDeals from '../components/Beds/BedsDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <BedsDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
