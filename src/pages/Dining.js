0dddd25aca8844d5fb9a2b49bb823f768f9a28b1
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 18:24:02 +0300

    feat: add Camera component to display dining deals with Navbar and Footer

---
+++ src/pages/Dining.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import DiningDeals from '../components/Dining/DiningDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <DiningDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
