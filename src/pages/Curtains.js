80cf64bf7397c7bd3c8f5d6954e2280ddd594e6d
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 21:49:39 +0300

    feat: create Camera component to display curtains deals with routing

---
+++ src/pages/Curtains.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import CurtainsDeals from '../components/Curtains/CurtainsDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <CurtainsDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
