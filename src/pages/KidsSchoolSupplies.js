e19eed5cde924c6a74b2ad19f625b0bca156a169
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 17:50:36 +0300

    feat: implement Mobile component for displaying kids school supplies deals with Navbar and Footer

---
+++ src/pages/KidsSchoolSupplies.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import KidsSchoolSuppliesDeals from '../components/KidsSchoolSupplies/KidsSchoolSuppliesDeals';
+import Navbar from '../components/Navbar';
+
+const Mobile = () => {
+    return(
+        <div>
+            <Navbar />
+            <KidsSchoolSuppliesDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Mobile;
