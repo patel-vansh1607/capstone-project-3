398dbefac959421f30a8e34efd463c82a9dce1a4
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 21:57:40 +0300

    feat: add laptop data to fitnessdata.js for display and routing

---
+++ src/data/fitnessdata.js
@@ -0,0 +1,54 @@
+const laptop = [
+   {
+    id: "adjustable-dumbbells",
+    name: "Adjustable Dumbbell Set (20kg)",
+    price: 1899,
+    originalPrice: 2499,
+    discount: "-24%",
+    brand: "FitKit",
+    rating: 4.6,
+    image: "adjustable-dumbbells.jpg"
+  },
+  {
+    id: "resistance-bands",
+    name: "Resistance Band Set (5 Bands)",
+    price: 799,
+    originalPrice: 999,
+    discount: "-20%",
+    brand: "Boldfit",
+    rating: 4.4,
+    image: "resistance-bands.jpg"
+  },
+  {
+    id: "yoga-mat",
+    name: "Yoga Mat (6mm, Anti-Slip)",
+    price: 499,
+    originalPrice: 699,
+    discount: "-29%",
+    brand: "Strauss",
+    rating: 4.5,
+    image: "yoga-mat.jpg"
+  },
+  {
+    id: "kettlebell-10kg",
+    name: "Kettlebell (10kg)",
+    price: 1199,
+    originalPrice: 1499,
+    discount: "-20%",
+    brand: "Reebok",
+    rating: 4.3,
+    image: "kettlebell-10kg.jpg"
+  },
+  {
+    id: "treadmill",
+    name: "Motorized Treadmill 2HP",
+    price: 25999,
+    originalPrice: 32999,
+    discount: "-21%",
+    brand: "PowerMax",
+    rating: 4.7,
+    image: "treadmill.jpg"
+  }
+];
+
+export default laptop;
