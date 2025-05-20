26085fd3e4e8518fe26e429fca39dd0c1bf29b3b
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-20 21:49:36 +0300

    feat: create Camera component to display books deals with routing

---
+++ src/pages/Books.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import BooksDeals from '../components/Books/BooksDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <BooksDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
