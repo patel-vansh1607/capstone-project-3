e1a7ba2b1afde41d9c956122e0fd518ac6b16bc2
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:34:06 +0300

    feat: add SecurityPage component

---
+++ src/pages/footer_pages/SecurityPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/SecurityPage.css"
+
+const SecurityPage = () => {
+    return(
+        <div>
+           <h1>Security Page</h1>
+        </div>
+    )
+}
+export default SecurityPage;
