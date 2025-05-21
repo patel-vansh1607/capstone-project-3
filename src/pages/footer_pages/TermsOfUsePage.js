0dc77f44f30785f6491e2f8b27573b4ee0efcafb
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:34:14 +0300

    feat: add TermsOfUsePage component

---
+++ src/pages/footer_pages/TermsOfUsePage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/TermsOfUsePage.css"
+
+const TermsOfUsePage = () => {
+    return(
+        <div>
+           <h1>TermsOfUse Page</h1>
+        </div>
+    )
+}
+export default TermsOfUsePage;
