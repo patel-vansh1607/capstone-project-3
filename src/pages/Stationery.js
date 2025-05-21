45986ccffdbcc8d3127b7850c9bcb3c936dedaa7
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:16:59 +0300

    feat: create Camera component to display stationery deals with navbar and footer

---
+++ src/pages/Stationery.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import StationeryDeals from '../components/Stationery/StationeryDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <StationeryDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
