9665efaa7dd8dfdd4111fa4401a7ed60f27b5677
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:43 +0300

    feat: add Cancellation and Return page component

---
+++ src/pages/footer_pages/Cancellation_ReturnPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/Cancellation_ReturnPage.css"
+
+const Cancellation_ReturnPage = () => {
+    return(
+        <div>
+           <h1>Payementsand cancel Page</h1>
+        </div>
+    )
+}
+export default Cancellation_ReturnPage;
