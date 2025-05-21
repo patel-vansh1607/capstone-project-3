52ffd55212507a4a3d6315479cc3465dd70a0483
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:32:49 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/WomenJewellery.js
+++ src/pages/WomenJewellery.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import WomensJewelleryDeals from '../components/WomensJewellery/WomensJewelleryDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Mobile = () => {
     return(
         <div>
             <Navbar />
+            <NavList />
             <WomensJewelleryDeals />
             <Footer />
         </div>
