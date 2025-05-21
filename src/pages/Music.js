f2d487fbf5f47a0b83f0d4a649b9a4b7ab00a846
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:32:29 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import MusicDeals from '../components/Music/MusicDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Camera = () => {
     return(
         <div>
             <Navbar />
+            <NavList />
             <MusicDeals />
             <Footer />
         </div>
