87bb930b6521521965afd4113121bf26fd75e10f
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:59:58 +0300

    fix: format code for better readability in index.js

--- src/index.js
+++ src/index.js
@@ -298,9 +298,10 @@ const router = createBrowserRouter([
   }
 ])
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
