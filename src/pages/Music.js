17abda9dae9e675e52ed1541d8e60097535f26ab
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:16:55 +0300

    feat: create Music.js component to display music deals with navbar and footer

---
+++ src/pages/Music.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import MusicDeals from '../components/Music/MusicDeals';
+import Navbar from '../components/Navbar';
+
+const Camera = () => {
+    return(
+        <div>
+            <Navbar />
+            <MusicDeals />
+            <Footer />
+        </div>
+    )
+}
+export default Camera;
