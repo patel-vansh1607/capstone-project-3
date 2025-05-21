646323e98b4bdd53fdb938c9131b3fd30c5319b8
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:32:32 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/Refrigerator.js
+++ src/pages/Refrigerator.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import RefrigeratorDeals from '../components/Refrigerator/RefrigeratorDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Camera = () => {
     return(
         <div>
             <Navbar />
+            <NavList />
             <RefrigeratorDeals />
             <Footer />
         </div>
