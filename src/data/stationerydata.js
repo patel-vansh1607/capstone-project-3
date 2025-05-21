91d50f666331c6d72146a894b73debab70e4a4f5
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:16:49 +0300

    feat: add laptop data to stationerydata.js for detailed product listings

---
+++ src/data/stationerydata.js
@@ -0,0 +1,54 @@
+const laptop = [
+   {
+    id: "gel-pen-set",
+    name: "Pack of 10 Gel Pens",
+    price: 129,
+    originalPrice: 199,
+    discount: "-35%",
+    brand: "Cello",
+    rating: 4.4,
+    image: "gel-pen-set.jpg"
+  },
+  {
+    id: "notebook-set",
+    name: "A5 Spiral Notebook Set (Pack of 3)",
+    price: 249,
+    originalPrice: 349,
+    discount: "-29%",
+    brand: "Classmate",
+    rating: 4.6,
+    image: "notebook-set.jpg"
+  },
+  {
+    id: "highlighter-pack",
+    name: "Highlighter Pens (Pack of 5)",
+    price: 179,
+    originalPrice: 249,
+    discount: "-28%",
+    brand: "Faber-Castell",
+    rating: 4.3,
+    image: "highlighters.jpg"
+  },
+  {
+    id: "geometry-box",
+    name: "Complete Geometry Box",
+    price: 89,
+    originalPrice: 129,
+    discount: "-31%",
+    brand: "Camlin",
+    rating: 4.5,
+    image: "geometry-box.jpg"
+  },
+  {
+    id: "desk-organizer",
+    name: "Multi-section Desk Organizer",
+    price: 399,
+    originalPrice: 499,
+    discount: "-20%",
+    brand: "Amazon Basics",
+    rating: 4.7,
+    image: "desk-organizer.jpg"
+  }
+];
+
+export default laptop;
