88d3da5978a5a0b6a68ad7c2e85decc5b2139caa
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:37 +0300

    feat: add Aboutus page component

---
+++ src/pages/footer_pages/AboutusPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/AboutusPage.css"
+
+const AboutusPage = () => {
+    return(
+        <div>
+           <h1>Aboutus Page</h1>
+        </div>
+    )
+}
+export default AboutusPage;
