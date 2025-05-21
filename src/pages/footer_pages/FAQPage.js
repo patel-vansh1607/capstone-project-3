955ea050ad7e6c29870b48a69f9b78f0411a8cf5
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:53 +0300

    feat: add FAQPage component

---
+++ src/pages/footer_pages/FAQPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/FAQPage.css"
+
+const FAQPage = () => {
+    return(
+        <div>
+           <h1>Carrers Page</h1>
+        </div>
+    )
+}
+export default FAQPage;
