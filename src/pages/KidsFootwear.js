4b1ba9a120aefc1bc4ac1ac4d428730eb425e1d6
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:31:39 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/KidsFootwear.js
+++ src/pages/KidsFootwear.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import KidsFootwearDeals from '../components/KidsFootwear/KidsFootwearDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Mobile = () => {
     return(
         <div>
             <Navbar />
+            <NavList  />
             <KidsFootwearDeals />
             <Footer />
         </div>
