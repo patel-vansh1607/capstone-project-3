cc0a5b22f4aa912633b74b35803ac7ab5fe64f37
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:49 +0300

    feat: add ContactPage component

---
+++ src/pages/footer_pages/ContactPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/ContactPage.css"
+
+const ContactPage = () => {
+    return(
+        <div>
+           <h1>Contact Page</h1>
+        </div>
+    )
+}
+export default ContactPage;
