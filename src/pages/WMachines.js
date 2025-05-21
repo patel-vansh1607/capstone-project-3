2216a57d81db72f6de4b08554ab1a9a4fe814ccb
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:32:47 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/WMachines.js
+++ src/pages/WMachines.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import WMachinesDeals from '../components/WMachines/WMachinesDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Mobile = () => {
     return(
         <div>
             <Navbar />
+            <NavList />
             <WMachinesDeals />
             <Footer />
         </div>
