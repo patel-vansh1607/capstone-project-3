6884ab440187f3178578b1bc20a45d53badae39d
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:32:37 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/Stationery.js
+++ src/pages/Stationery.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import StationeryDeals from '../components/Stationery/StationeryDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Camera = () => {
     return(
         <div>
             <Navbar />
+            <NavList />
             <StationeryDeals />
             <Footer />
         </div>
