a1f295435ada782684f23465de50cc05de4fe4d0
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 15:32:23 +0300

    feat: add Navlist component to Camera for improved navigation

--- src/pages/Microwave.js
+++ src/pages/Microwave.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import MicrowaveDeals from '../components/Microwave/MicrowaveDeals';
 import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
 
 const Camera = () => {
     return(
         <div>
             <Navbar />
+            <NavList />
             <MicrowaveDeals />
             <Footer />
         </div>
