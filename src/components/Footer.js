fd7a6f80bde973a76d5fcc3f4fa6b252a43f957a
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:32 +0300

    fix: update footer links and address information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,12 +8,9 @@ const Footer = () => {
         <div className="footer-section">
           <h4>ABOUT</h4>
           <ul>
-            <li><a href="/">Contact Us</a></li>
-            <li><a href="/">About Us</a></li>
-            <li><a href="/">Careers</a></li>
-            <li><a href="/">Flipkart Stories</a></li>
-            <li><a href="/">Press</a></li>
-            <li><a href="/">Corporate Information</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="/about-us">About Us</a></li>
+            <li><a href="/careers">Careers</a></li>
           </ul>
         </div>
         <div className="footer-section">
@@ -27,57 +24,53 @@ const Footer = () => {
         <div className="footer-section">
           <h4>HELP</h4>
           <ul>
-            <li><a href="/">Payments</a></li>
-            <li><a href="/">Shipping</a></li>
-            <li><a href="/">Cancellation & Returns</a></li>
-            <li><a href="/">FAQ</a></li>
+            <li><a href="/payements">Payments</a></li>
+            <li><a href="/shipping">Shipping</a></li>
+            <li><a href="/cancellation-and-returns">Cancellation & Returns</a></li>
+            <li><a href="/faq">FAQ</a></li>
           </ul>
         </div>
         <div className="footer-section">
           <h4>CONSUMER POLICY</h4>
           <ul>
-            <li><a href="/">Cancellation & Returns</a></li>
-            <li><a href="/">Terms Of Use</a></li>
-            <li><a href="/">Security</a></li>
-            <li><a href="/">Privacy</a></li>
-            <li><a href="/">Sitemap</a></li>
-            <li><a href="/">Grievance Redressal</a></li>
-            <li><a href="/">EPR Compliance</a></li>
+            <li><a href="/cancellation-and-returns">Cancellation & Returns</a></li>
+            <li><a href="/terms-of-use">Terms Of Use</a></li>
+            <li><a href="/security">Security</a></li>
+            <li><a href="/privacy">Privacy</a></li>
           </ul>
         </div>
         <div className="footer-section address">
           <p><strong>Mail Us:</strong><br />
-            Flipkart Internet Private Limited,<br />
-            Buildings Alyssa, Begonia &<br />
-            Clove Embassy Tech Village,<br />
-            Outer Ring Road, Devarabeesanahalli Village,<br />
-            Bengaluru, 560103,<br />
-            Karnataka, India
+            Technologia Internet Private Limited,<br />
+            Buildings Nakuru, Nairobi &<br />
+            Embassy  Village,<br />
+            Outer Ring Road, Kenya ,<br />
+            Nakuru, 20100,<br />
+            Nairobi, Kenya
           </p>
           <p><strong>Registered Office Address:</strong><br />
-            Flipkart Internet Private Limited,<br />
-            Buildings Alyssa, Begonia &<br />
-            Clove Embassy Tech Village,<br />
-            Outer Ring Road, Devarabeesanahalli Village,<br />
-            Bengaluru, 560103,<br />
-            Karnataka, India<br />
-            CIN: U51109KA2012PTC066107<br />
-            Telephone: <span className="highlight">044-45614700 / 044-67415800</span>
+            Technologia Internet Private Limited,<br />
+            Buildings Nakuru, Nairobi &<br />
+            Embassy  Village,<br />
+            Outer Ring Road, Kenya ,<br />
+            Nakuru, 20100,<br />
+            Nairobi, Kenya<br />
+            Telephone: <span className="highlight">+254 (0) 748 660 944</span>
           </p>
         </div>
       </div>
       <div className="footer-bottom">
         <div className="footer-links">
-          <span>🚀 Become a Seller</span>
-          <span>📣 Advertise</span>
-          <span>🎁 Gift Cards</span>
-          <span>❓ Help Center</span>
+          <span>Become a Seller</span>
+          <span>Advertise</span>
+          <span>Gift Cards</span>
+          <span>Help Center</span>
           <span>© 2025 Technologia.com</span>
         </div>
         <div className="footer-payments">
-          <img src="https://img.icons8.com/color/48/000000/visa.png" alt="Visa" />
+          {/* <img src="https://img.icons8.com/color/48/000000/visa.png" alt="Visa" />
           <img src="https://img.icons8.com/color/48/000000/mastercard.png" alt="Mastercard" />
-          <img src="https://img.icons8.com/color/48/000000/rupay.png" alt="RuPay" />
+          <img src="https://img.icons8.com/color/48/000000/rupay.png" alt="RuPay" /> */}
           {/* Add more icons as needed */}
         </div>
       </div>
