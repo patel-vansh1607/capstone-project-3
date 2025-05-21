c7debbc7b04602c5d65f80c805de1cfa3c1a713e
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:58 +0300

    feat: add PayementsPage component

---
+++ src/pages/footer_pages/PayementsPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/PayementsPage.css"
+
+const PayementsPage = () => {
+    return(
+        <div>
+           <h1>Payements Page</h1>
+        </div>
+    )
+}
+export default PayementsPage;
