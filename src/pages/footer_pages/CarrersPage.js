50eb262419ea5d5286c944aec358a6a93803b93a
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:33:47 +0300

    feat: add CarrersPage component

---
+++ src/pages/footer_pages/CarrersPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/CarrersPage.css"
+
+const CarrersPage = () => {
+    return(
+        <div>
+           <h1>Carrers Page</h1>
+        </div>
+    )
+}
+export default CarrersPage;
