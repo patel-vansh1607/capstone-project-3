c04fa0f8a4cdc218d68470f986dea731aef7d204
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:34:11 +0300

    feat: add ShippingPage component

---
+++ src/pages/footer_pages/ShippingPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/ShippingPage.css"
+
+const ShippingPage = () => {
+    return(
+        <div>
+           <h1>Shipping Page</h1>
+        </div>
+    )
+}
+export default ShippingPage;
