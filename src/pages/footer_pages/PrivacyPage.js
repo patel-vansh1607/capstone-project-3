d2108b3d194900412d1982a1edf3b84f1308e16f
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-21 21:34:03 +0300

    feat: add PrivacyPage component

---
+++ src/pages/footer_pages/PrivacyPage.js
@@ -0,0 +1,10 @@
+import "../../styles/footer_pages/PrivacyPage.css"
+
+const PrivacyPage = () => {
+    return(
+        <div>
+           <h1>Privacy Page</h1>
+        </div>
+    )
+}
+export default PrivacyPage;
