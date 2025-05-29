5302f16d8df09478896a0efeb168e0aa69a2f77e
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-29 21:29:28 +0300

    feat: create TVApp component with layout and navigation

---
+++ src/pages/TVApp.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
+import TvAppp from '../components/tvapp';
+
+const TVApp = () => {
+    return(
+        <div>
+            <Navbar />
+            <NavList />
+            <TvAppp />
+            <Footer />
+        </div>
+    )
+}
+export default TVApp;
