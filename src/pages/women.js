d48f8198fac841ddaa33dd2510d4569e4a159422
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-29 21:29:31 +0300

    feat: add Women component with layout and navigation

---
+++ src/pages/women.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
+import Womenp from '../components/women';
+
+const Women = () => {
+    return(
+        <div>
+            <Navbar />
+            <NavList />
+            <Womenp />
+            <Footer />
+        </div>
+    )
+}
+export default Women;
