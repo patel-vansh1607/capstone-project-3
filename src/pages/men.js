bb2fcda81eede205280c3a44e94b65b7647b5810
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-29 21:29:21 +0300

    feat: add Men component with layout and navigation

---
+++ src/pages/men.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
+import Menn from '../components/men';
+
+const Men = () => {
+    return(
+        <div>
+            <Navbar />
+            <NavList />
+            <Menn />
+            <Footer />
+        </div>
+    )
+}
+export default Men;
