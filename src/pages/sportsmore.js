31a9588222064bf8065da11bdf9bc76402fe5118
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-29 21:29:24 +0300

    feat: add SportMore component with layout and navigation

---
+++ src/pages/sportsmore.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
+import SportMoree from '../components/sportmore';
+
+const SportMore = () => {
+    return(
+        <div>
+            <Navbar />
+            <NavList />
+            <SportMoree />
+            <Footer />
+        </div>
+    )
+}
+export default SportMore;
