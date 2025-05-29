ddec32e7504cdad5df8c2d773866831a32960abc
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-29 21:29:17 +0300

    feat: create HomeFuniture component with layout and navigation

---
+++ src/pages/homefuniture.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
+import HomeFun from '../components/homefun';
+
+const HomeFuniture = () => {
+    return(
+        <div>
+            <Navbar />
+            <NavList />
+            <HomeFun />
+            <Footer />
+        </div>
+    )
+}
+export default HomeFuniture;
