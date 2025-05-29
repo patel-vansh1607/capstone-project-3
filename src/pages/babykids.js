cce82f48df06c4cb70ffda4d186f88ef7cd029d2
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-29 21:29:14 +0300

    feat: add BabyKids component with layout and navigation

---
+++ src/pages/babykids.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NavList from '../components/Navlist';
+import BabyKidss from '../components/babykids';
+
+const BabyKids = () => {
+    return(
+        <div>
+            <Navbar />
+            <NavList />
+            <BabyKidss />
+            <Footer />
+        </div>
+    )
+}
+export default BabyKids;
