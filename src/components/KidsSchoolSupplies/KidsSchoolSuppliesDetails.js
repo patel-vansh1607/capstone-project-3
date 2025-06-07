19f530872df640076812842279e5e2a00ff99a3f
Vansh Patel <patel.vansh1607@gmail.com>
2025-06-07 09:26:51 +0300

    feat: add Navbar and Footer components to KidsSchoolSuppliesDetails for improved layout

--- src/components/KidsSchoolSupplies/KidsSchoolSuppliesDetails.js
+++ src/components/KidsSchoolSupplies/KidsSchoolSuppliesDetails.js
@@ -3,6 +3,8 @@ import { useParams, Link } from "react-router-dom";
 import phones from "../../data/kidsschoolsuppliesdata";
 import "../../styles/MobileDeals.css";
 import { useCart } from "../../context/CartContext";
+import Footer from "../Footer";
+import Navbar from "../Navbar";
 
 
 const PhoneDetails = () => {
@@ -16,7 +18,9 @@ const PhoneDetails = () => {
   if (!phone) return <h2>Supplies not found</h2>;
 
   return (
-    <div className="phone-details">
+    <div>
+      <Navbar />
+      <div className="phone-details">
       <Link to="/" className="back-link">← Back to deals</Link>
       <div className="details-container">
         <img src={phone.image} alt={phone.name} className="detail-image" />
@@ -30,6 +34,8 @@ const PhoneDetails = () => {
           <button className="buy-btn" onClick={() => addToCart(phone)}>Add to cart</button>
         </div>
       </div>
+    </div>
+    <Footer />
     </div>
   );
 };
