94ebbf5fa8037c2a3cb824c7fe9582e5131ba1a6
Vansh Patel <patel.vansh1607@gmail.com>
2025-06-13 14:40:15 +0300

    fix: update ShippingPage layout and content for improved clarity and user engagement

--- src/pages/footer_pages/ShippingPage.js
+++ src/pages/footer_pages/ShippingPage.js
@@ -1,9 +1,22 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 import "../../styles/footer_pages/ShippingPage.css"
 
 const ShippingPage = () => {
     return(
         <div>
-           <h1>Shipping Page</h1>
+            <Navbar />
+           <div className="info-page">
+            <h2>Shipping Information</h2>
+            <p>We provide reliable and fast delivery all across India:</p>
+            <ul>
+                <li>Free shipping on orders above ₹999</li>
+                <li>Delivery Time: 2–7 business days depending on your location</li>
+                <li>Real-time tracking via order page</li>
+                <li>Partnered with top courier services</li>
+            </ul>
+            </div>
+            <Footer />
         </div>
     )
 }
