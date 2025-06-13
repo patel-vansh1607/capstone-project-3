0537461e8d814368f5d248a2965095cc2bd81f15
Vansh Patel <patel.vansh1607@gmail.com>
2025-06-13 14:39:59 +0300

    fix: update Cancellation_ReturnPage layout and content for improved clarity and user experience

--- src/pages/footer_pages/Cancellation_ReturnPage.js
+++ src/pages/footer_pages/Cancellation_ReturnPage.js
@@ -1,9 +1,22 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 import "../../styles/footer_pages/Cancellation_ReturnPage.css"
 
 const Cancellation_ReturnPage = () => {
     return(
         <div>
-           <h1>Payementsand cancel Page</h1>
+            <Navbar />
+             <div className="info-page">
+                <h2>Cancellation & Returns</h2>
+                <p>We want you to love your purchase. Here's our return policy:</p>
+                <ul>
+                    <li>Cancel orders within 12 hours of placing them</li>
+                    <li>Return or exchange within 7 days of delivery</li>
+                    <li>Refunds processed within 5–7 business days</li>
+                    <li>Items must be unused and in original condition</li>
+                </ul>
+                </div>
+                <Footer />
         </div>
     )
 }
